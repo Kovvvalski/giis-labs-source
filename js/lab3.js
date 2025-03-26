cbd56c5103cba85e255503093b41c2c6a57cbd69
Daniil Bylkou <daniil.bylkou@innowise.com>
2025-03-26 10:46:57 +0300

    vectors view

--- js/lab3.js
+++ js/lab3.js
@@ -60,13 +60,22 @@ document.addEventListener("DOMContentLoaded", () => {
 
     canvas.addEventListener("dblclick", (event) => {
         let newPoint = { x: event.offsetX, y: event.offsetY };
+        let existingPoint = points.find(p => Math.hypot(p.x - newPoint.x, p.y - newPoint.y) < 10);
+
+        if (existingPoint) {
+            points = points.map(p => (p === existingPoint ? existingPoint : p));
+            bspline.points = bspline.points.map(p => (p === existingPoint ? existingPoint : p));
+        } else {
+            points.push(newPoint);
+            existingPoint = newPoint;
+        }
+
         if (curveType.value === "bspline") {
-            bspline.addPoint(newPoint);
+            bspline.addPoint(existingPoint);
             if (bspline.points.length >= 4) {
                 curves.push(new Curve(...bspline.points.slice(-4)));
             }
         } else {
-            points.push(newPoint);
             if (points.length % 4 === 0) {
                 curves.push(new Curve(...points.slice(-4)));
             }
@@ -92,10 +101,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
             if (curveType.value === "hermite") {
                 curvePoints = hermiteCurve(curve.points);
+                drawVectors(curve.points[0], curve.points[2]);
+                drawVectors(curve.points[1], curve.points[3]);
             } else if (curveType.value === "bezier") {
                 curvePoints = bezierCurve(curve.points);
+                drawVectors(curve.points[0], curve.points[1]);
+                drawVectors(curve.points[2], curve.points[3]);
             } else if (curveType.value === "bspline") {
                 curvePoints = bsplineCurve(curve.points);
+                for (let i = 0; i < curve.points.length - 1; i++) {
+                    drawVectors(curve.points[i], curve.points[i + 1]);
+                }
             }
 
             if (curvePoints.length > 0) {
@@ -104,6 +120,15 @@ document.addEventListener("DOMContentLoaded", () => {
                 ctx.stroke();
             }
         });
+    }
+
+    function drawVectors(p1, p2) {
+        ctx.strokeStyle = "green";
+        ctx.beginPath();
+        ctx.moveTo(p1.x, p1.y);
+        ctx.lineTo(p2.x, p2.y);
+        ctx.stroke();
+        ctx.strokeStyle = "blue";
     }
 
 
